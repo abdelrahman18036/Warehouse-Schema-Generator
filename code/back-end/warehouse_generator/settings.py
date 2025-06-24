a0849c8b03bd1cf4caa0b838b994e8b8003fdea9
Abdelrahman Abdelnasser Gamal <2021170298@cis.asu.edu.eg>
2025-06-25 00:10:45 +0300

    update

--- code/back-end/warehouse_generator/settings.py
+++ code/back-end/warehouse_generator/settings.py
@@ -93,10 +93,21 @@ WSGI_APPLICATION = 'warehouse_generator.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'scheme',
+        'USER': 'postgres',
+        'PASSWORD': '00oo00oo',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
